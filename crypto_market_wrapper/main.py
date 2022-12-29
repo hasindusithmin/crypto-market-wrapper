9bd2177e1c2a9fc18b601418fcb1cf4366e51bbd
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-29 21:43:56 +0530

    Update main.py (define GET_AGGREGATE_TRADES_LIST.)

--- crypto_market_wrapper/main.py
+++ crypto_market_wrapper/main.py
@@ -29,6 +29,29 @@ def GET_RECENT_TRADES_LIST(SYMBOL):
         "IS_BEST_MATCH":IS_BEST_MATCH
     })
 
-df = GET_RECENT_TRADES_LIST('btcusd')
+def GET_AGGREGATE_TRADES_LIST(SYMBOL):
+    URL = f'https://www.binance.com/api/v3/aggTrades?symbol={SYMBOL.upper()}' 
+    RES = requests.get(URL)
+    if RES.status_code != 200:
+        return "It has been determined that the symbol provided is invalid. Please utilize the GET_SYMBOLS function to retrieve a list of valid symbols, and display them for reference."
+    AGGREGATE_TRADES_LIST = RES.json()
+    AGGREGATE_TRADE_ID = [AGGREGATE_TRADE['a'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    TIMESTAMP = [AGGREGATE_TRADE['T'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    PRICE = [AGGREGATE_TRADE['p'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    QUANTITY = [AGGREGATE_TRADE['q'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    FIRST_TRADE_ID = [AGGREGATE_TRADE['f'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    LAST_TRADE_ID = [AGGREGATE_TRADE['l'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    WAS_THE_BUYER_THE_MAKER = [AGGREGATE_TRADE['m'] for AGGREGATE_TRADE in AGGREGATE_TRADES_LIST]
+    return DataFrame({
+        "AGGREGATE_TRADE_ID":AGGREGATE_TRADE_ID,
+        "TIMESTAMP":TIMESTAMP,
+        "PRICE":PRICE,
+        "QUANTITY":QUANTITY,
+        "FIRST_TRADE_ID":FIRST_TRADE_ID,
+        "LAST_TRADE_ID":LAST_TRADE_ID,
+        "WAS_THE_BUYER_THE_MAKER":WAS_THE_BUYER_THE_MAKER
+    })
+
+df = GET_AGGREGATE_TRADES_LIST('btcusdt')
 print(df)
 
