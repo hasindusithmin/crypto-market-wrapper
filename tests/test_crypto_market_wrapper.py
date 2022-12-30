e3fa63895f3a3e04e7eeb09c1bbdfa3a9c4f9e9e
hasindusithmin <hasindusithmin64@gmail.com>
2022-12-30 09:05:48 +0530

    Rename main.py->crypto.py

--- tests/test_crypto_market_wrapper.py
+++ tests/test_crypto_market_wrapper.py
@@ -1,5 +1,14 @@
 from crypto_market_wrapper import __version__
-
-
+from crypto_market_wrapper import crypto
+import requests
+import urllib
+from pandas import DataFrame
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_GET_SYMBOLS():
+    SYMBOLS = crypto.GET_SYMBOLS()
+    URL = 'https://www.binance.com/api/v3/exchangeInfo'
+    RES = requests.get(URL)
+    DATA = RES.json()
+    assert len(SYMBOLS) == len(DataFrame({'SYMBOLS': [SYMBOL['symbol'] for SYMBOL in DATA['symbols']]}))
